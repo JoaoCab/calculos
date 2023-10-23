9b5ec7265eb19f7533d5b80b17ccbd3888c699e9
JoaoCab <146043580+JoaoCab@users.noreply.github.com>
2023-10-23 20:34:42 +0100

    Adicionar método ConverterTemperatura().

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Calculos
 {
     /// <summary>
+    /// Tipo de conversão de temperatura a executar.
+    /// </summary>
+    public enum ConversaoTemperatura
+    {
+        Nulo = 0,
+        CelsiusFahrenheit,
+        FahrenheitCelsius
+    }
+    /// <summary>
     /// Implementa operações aritméticas.
     /// </summary>
     internal class Aritmetica
@@ -26,6 +35,20 @@ namespace Calculos
         public int Subtrair(int x, int y)
         {
             return x - y;
+        }
+
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if(conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if(conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return (temperatura - 32) * 1.8000;
+            }
+
+            return -1;
         }
     }
 }
